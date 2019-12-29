54d7e3c6d40a7baa65dc147e850145ef1ed346de
Erick Chiliboyi <sirchikoz@gmail.com>
2019-12-29 00:08:30 -0500

    Started Dijkstra

---
+++ AdjacentMatrixShortTestPath/src/adjacentmatrixshorttestpath/Dijkstra.java
@@ -0,0 +1,10 @@
+
+package adjacentmatrixshorttestpath;
+
+/**
+ *This is an implementation of the Dijkstra's algorithm for the shortest path
+ * @author sirch
+ */
+public class Dijkstra {
+    
+}
